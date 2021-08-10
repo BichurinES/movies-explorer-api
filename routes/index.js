128e1b50e48490aa5ac4769517685a9035756742
Egor Bichurin <bichurin.es@gmail.com>
2021-08-10 20:13:32 +0500

    refactor: удалён лишний мидлвар

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,23 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const auth = require('../middlewares/auth');
 const { createUser, login, logout } = require('../controllers/users');
-const { signupValidator, signinValidator } = require('../middlewares/validate-sign-routes');
 
-router.post('/signup', signupValidator, createUser);
-router.post('/signin', signinValidator, login);
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().required().min(2).max(30),
+  }),
+}), createUser);
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
 router.post('/signout', auth, logout);
+
 router.use('/users', auth, require('./users'));
 router.use('/movies', auth, require('./movies'));
 router.use('*', auth, require('./others'));
