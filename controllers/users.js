67f13355a5dcf9987e6a67164fc260b649f87e0f
Egor Bichurin <bichurin.es@gmail.com>
2021-09-22 17:21:02 +0500

    fix: исправления после связки с фронтом

--- controllers/users.js
+++ controllers/users.js
@@ -87,6 +87,7 @@ module.exports.login = (req, res, next) => {
         res.cookie('jwt', token, {
           httpOnly: true,
           maxAge: 3600000 * 24 * 7,
+          sameSite: 'none',
         }).send({ message: LOGIN_MSG });
       }))
     .catch((err) => {
