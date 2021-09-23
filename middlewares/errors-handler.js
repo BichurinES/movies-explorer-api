d252c3892089fb3508ebd803f19a75ab1d972053
Egor Bichurin <bichurin.es@gmail.com>
2021-09-23 14:00:40 +0500

    fix: исправления после связки с фронтом

--- middlewares/errors-handler.js
+++ middlewares/errors-handler.js
@@ -4,6 +4,6 @@ module.exports = (err, req, res, next) => {
   const statusCode = err.statusCode ? err.statusCode : 500;
   const message = statusCode === 500 ? DEFAULT_ERR_MSG : err.message;
 
-  res.status(statusCode).send({ message });
+  res.status(statusCode).send(err);
   next();
 };
